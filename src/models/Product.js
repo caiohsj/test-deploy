15afd643e695c8031ee6e4b8f3739300bd6ec3a5
caiohsj <caiocoxim10@gmail.com>
2021-11-17 19:48:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+});
+
+mongoose.plugin(mongoosePaginate)
+
+mongoose.model('Product', ProductSchema);
